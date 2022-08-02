b45d6a55aa99becbde239bc1f141454fa85c8a28
orsi-crey <ok@creygames.com>
2022-08-02 21:09:39 +0200

    Add two buttons for navigation. wow

--- src/routes/auth/auth.component.tsx
+++ src/routes/auth/auth.component.tsx
@@ -1,6 +1,20 @@
+import { Button } from "@react-md/button";
+import { useNavigate } from "react-router-dom";
+
 const Auth = () => {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/");
+  }
+
   return (
-    <div>This will be the login page!</div>
+    <>
+      <div>This will be the login page!</div>
+      <Button theme="primary" themeType="outline" onClick={handleClick}>
+        Go back home
+      </Button>
+    </>
   )
 }
 
--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@react-md/button";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/auth");
+  }
+
   return (
     <>
       <div>Hi!</div>
-      <Button theme="primary" themeType="contained">
+      <Button theme="primary" themeType="contained" onClick={handleClick}>
         Click to log in
       </Button>
     </>
