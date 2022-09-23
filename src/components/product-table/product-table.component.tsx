f4f5aef11b40ffdf24e39b19c3fb9d1db6e75f39
orsi-crey <ok@creygames.com>
2022-09-23 21:18:04 +0200

    Add autocomplete for brands on polish page

--- src/components/product-table/product-table.component.tsx
+++ src/components/product-table/product-table.component.tsx
@@ -1,9 +1,23 @@
-import { Form, TextField } from 'react-md';
+import { AutoComplete, Form, TextField } from 'react-md';
 import { ProductTableProps } from '../../routes/product-page/product.component';
+import { getListFilteredFieldsQuery } from '../../utils/firestore/firestore.utils';
 
 import ChipField from '../chip-field/chip-field.component';
 
 const ProductTable = ({ productId, product, editable, setproduct }: ProductTableProps) => {
+  const allBrandsQuery = getListFilteredFieldsQuery('products', 'brand');
+
+  const sortBrands = () => {
+    if (allBrandsQuery && allBrandsQuery.isSuccess && allBrandsQuery.data) {
+      const allBrands = allBrandsQuery.data;
+      return allBrands.sort().filter((item, pos, ary) => {
+        return !pos || item != ary[pos - 1];
+      });
+    } else {
+      return [];
+    }
+  };
+
   const setEffectChipsFromChild = (chips: string[]) => {
     setproduct({ ...product, effects: chips });
   };
@@ -26,11 +40,18 @@ const ProductTable = ({ productId, product, editable, setproduct }: ProductTable
         value={productId}
       />
       <p>Brand: (required)</p>
-      <TextField
+      <AutoComplete
         id="brand"
         name="Brand"
         disabled={!editable}
         value={product.brand}
+        data={sortBrands()}
+        onAutoComplete={(event) => {
+          setproduct({
+            ...product,
+            brand: event.value,
+          });
+        }}
         onChange={(event) => {
           setproduct({
             ...product,
