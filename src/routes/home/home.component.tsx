8e7faee813c194a972681f639fc3c30ff1c81137
orsi-crey <ok@creygames.com>
2022-10-02 23:18:02 +0200

    Change home component to show actual polish names

--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -4,11 +4,23 @@ import { MediaContainer } from 'react-md';
 import { Link, useNavigate } from 'react-router-dom';
 
 import { authState, UserContext } from '../../contexts/user.context';
+import { getListQuery } from '../../utils/firestore/firestore.utils';
 import { HomeContainer } from './home.styles';
 
 const Home = () => {
   const navigate = useNavigate();
   const { isLoggedIn, username, userdata } = useContext(UserContext);
+  const productsQuery = getListQuery('products');
+
+  const favoritePolishes = () => {
+    return productsQuery?.data?.docs
+      .filter((doc) => userdata.favorites.includes(doc.id))
+      .map((doc) => (
+        <div key={doc.id}>
+          <Link to={`/products/${doc.id}`}>{`${doc.data().brand} - ${doc.data().name}`}</Link>
+        </div>
+      ));
+  };
 
   const homeLinks = () => {
     switch (isLoggedIn) {
@@ -33,14 +45,8 @@ const Home = () => {
         return (
           <div>
             <div>Hi {username}!</div>
-            <div>Your favorite polishes: </div>
-            {userdata.favorites.map((item) => {
-              return (
-                <div key={item}>
-                  <Link to={`/products/${item}`}>{item}</Link>
-                </div>
-              );
-            })}
+            {productsQuery && <div>Your favorite polishes: </div>}
+            {userdata.favorites.length > 0 ? productsQuery && favoritePolishes() : 'No favorite polishes yet!'}
           </div>
         );
 
